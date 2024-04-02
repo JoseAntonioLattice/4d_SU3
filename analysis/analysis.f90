1d0610fb21131fcea0740203ed870f96ce39bf67
pepe_icn <joseantoniogarcia@ciencias.unam.mx>
2024-04-02 15:23:32 -0600

    Auto correlation time computed

--- analysis/analysis.f90
+++ analysis/analysis.f90
@@ -15,7 +15,7 @@ program analysis
   namelist /input_parameters/ L, N_thermalization, N_measurements, N_skip, algorithm, equilibrium
 
   integer(i4) :: inunit
-  character(256) :: directory 
+  character(256) :: data_file, data 
   
   type observable
      real(dp), allocatable :: array(:)
@@ -25,26 +25,62 @@ program analysis
 
   type(observable) :: Ep
 
-  integer(i4) :: i
+  integer(i4) :: i, i_beta, bins
+  real(dp), allocatable, dimension(:) :: beta, auto_correlation
+
+  beta = [(i*0.1_dp,i=1,80)]
   
   call read_input()
 
 
-  allocate(Ep%array(N_measurements))
-  
-  data_file = "data/L="//trim(int2str(L))//"/"//trim(eq)//"/"//trim(algorithm)&
-       //"/beta="//trim(beta(i_beta))
-  print*, trim(data_file)
-  open( newunit = inunit, file = trim(data_file) )
+  allocate(Ep%array(N_measurements),auto_correlation(N_measurements))
 
-  do i = 1, N_measurements
-     read(inunit,*) Ep%array(i)
+  data = "data/L="//trim(int2str(L))//"_equilibrium_"//trim(algorithm)//".dat"
+  open(unit = 666, file = trim(data), status = "unknown")
+  do i_beta = 1,size(beta)
+     data_file = "data/L="//trim(int2str(L))//"/"//"equilibrium"//"/"//trim(algorithm)&
+          //"/beta="//real2str(beta(i_beta))//"/observables.dat"
+     !print*, trim(data_file)
+     open( newunit = inunit, file = trim(data_file) )
+     
+     do i = 1, N_measurements
+        read(inunit,*) Ep%array(i)
+     end do
+     close(inunit)
+     
+     open(unit = 69, file = "data/L="//trim(int2str(L))//"/"//"equilibrium"//"/"//trim(algorithm)&
+          //"/beta="//real2str(beta(i_beta))//"/auto_correlation.dat")
+     auto_correlation = acf(Ep%array)
+     do i = 1, size(Ep%array)
+        write(69,*) auto_correlation(i)
+     end do
+     
+     call max_jackknife_error_2(Ep%array,Ep%avr,Ep%err,bins)
+     write(666,*) beta(i_beta),Ep%avr, Ep%err,bins
   end do
-
-  print*, Ep%array(1:5)
-  
 contains
 
+  function acf(x)
+    real(dp), intent(in), dimension(:) :: x
+    real(dp), dimension(0:size(x)-1) :: acf
+    integer(i4) :: k, n, t
+    real(dp) :: avr, var, suma
+    
+    n = size(x)
+
+    avr = sum(x)/n
+    var = (sum(x**2)-n*avr**2)/(n-1)
+    
+    do t = 0, n-1
+       suma = 0.0_dp
+       do k = 1, n-t
+          suma = suma + (x(k)-avr)*(x(k+t)-avr)
+       end do
+       acf(t) = 1/((n-t)*var)*suma
+    end do
+    
+  end function acf
+  
   subroutine read_input()
     integer(i4) :: in_unit
     character(20) :: parameters_file
--- run_analysis.sh
+++ run_analysis.sh
@@ -4,7 +4,10 @@ FC=gfortran
 
 SRC=analysis
 
-$FC -o bin/analysis.exe $SRC/analysis.f90 
+$FC -J bin -c src/statistics.f90 -o bin/statistics.o
+$FC -J bin -c src/number2string_mod.f90 -o bin/number2string_mod.o
+$FC -I bin -c $SRC/analysis.f90 -o bin/analysis.o
+$FC  bin/statistics.o bin/number2string_mod.o bin/analysis.o -o bin/analysis.exe  
 
 bin/analysis.exe <<< input_parameters.par
 
